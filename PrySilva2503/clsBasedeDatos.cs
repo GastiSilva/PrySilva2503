4f70f0577d8bb60360ea358f7dd0d8e6e25ec64d
root <mgiraudo@ies21.edu.ar>
2024-03-25 19:45:29 -0300

    Agregar archivos de proyecto.

---
+++ PrySilva2503/clsBasedeDatos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace PrySilva2503
+{
+    OleDbConnection conexionBD;
+    OleDbCommand comandoBD;
+    OleDbDataAdapter adaptadorDS;
+    DataSet objDataSet = new DataSet();
+    DirectoryInfo rutaProyecto = new DirectoryInfo(@"../..");
+    string cadenaConexion;
+    public bool estadoConexion;
+    internal class clsBasedeDatos
+    {
+
+    }
+
+    public void ConectarBaseDatos()
+    {
+        conexionBD = new OleDbConnection(cadenaConexion);
+        conexionBD.Open();
+        estadoConexion = true;
+    }
+}
